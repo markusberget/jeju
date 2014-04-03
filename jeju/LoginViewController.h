649428d25b40d1889b337ff5107fd36ad10d268e
Markus Berget <berget@student.chalmers.se>
2014-04-03 13:24:24 +0200

    Added LoginViewController

---
+++ jeju/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  jeju
+//
+//  Created by Markus Berget on 2014-04-03.
+//  Copyright (c) 2014 Markus Berget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
--- jeju/MasterViewController.m
+++ jeju/MasterViewController.m
@@ -27,8 +27,9 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    //Removed + button
+    //UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    //self.navigationItem.rightBarButtonItem = addButton;
 }
 
 - (void)didReceiveMemoryWarning
