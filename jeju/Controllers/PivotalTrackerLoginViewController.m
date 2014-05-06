49e33d8389ad6bba83af65e2b76cba0bb1e41423
Mathias Dosé <mathias.dose@hotmail.se>
2014-05-06 16:56:18 +0200

    Started on the Pivotal tracker authentication

---
+++ jeju/Controllers/PivotalTrackerLoginViewController.m
@@ -0,0 +1,49 @@
+//
+//  PivotalTrackerLoginViewController.m
+//  jeju
+//
+//  Created by Mathias Dosé on 06/05/14.
+//  Copyright (c) 2014 Markus Berget. All rights reserved.
+//
+
+#import "PivotalTrackerLoginViewController.h"
+
+@interface PivotalTrackerLoginViewController ()
+
+@end
+
+@implementation PivotalTrackerLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ jeju/Controllers/ProjectsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProjectsTableViewController.h
+//  jeju
+//
+//  Created by Mathias Dosé on 06/05/14.
+//  Copyright (c) 2014 Markus Berget. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectsTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *projects;
+
+
+
+@end
