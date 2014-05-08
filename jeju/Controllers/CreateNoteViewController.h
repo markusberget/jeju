e4a1658586f86f8b49ea8b74351869bc82a8c51a
Viktor Ansund <viktor@dhcp-205208.eduroam.chalmers.se>
2014-05-08 08:30:19 +0200

    CreateNoteClass added

---
+++ jeju/Controllers/CreateNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateNoteViewController.h
+//  
+//
+//  Created by Viktor Ansund on 07/05/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateNoteViewController : UIViewController
+
+@end
